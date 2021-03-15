0fee9ffb5ee79133e239698ff7d7f34152410d82
georgi <gogo249711@gmail.com>
2021-03-15 17:44:29 +0200

    Amend jenkinsfile to run docker

--- pylint.sh
+++ pylint.sh
@@ -2,6 +2,6 @@
 
 #!/bin/sh
 
-pylint /JenkinsTest/test.py
-result=$(grub-md5-crypt 2>&1 | grep xy)
-echo $result > output
+pylint /JenkinsTest/test.py > output.txt
+#result=$(grub-md5-crypt 2>&1 | grep xy)
+#echo $result > output
