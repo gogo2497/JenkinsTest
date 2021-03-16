a987b571fda7040c7924f8991c7cdb260be92948
georgi <gogo249711@gmail.com>
2021-03-16 11:51:58 +0200

    Amend jenkinsfile to run docker

--- pylint.sh
+++ pylint.sh
@@ -4,7 +4,7 @@
 
 out=$(pylint /JenkinsTest/test.py)
 echo $out
-if [[ $out =~ [0-9]]; then
+if [[ $out =~ [0-9] ]]; then
     echo "$out contains a digit"
 fi
 #result=$(grub-md5-crypt 2>&1 | grep xy)
