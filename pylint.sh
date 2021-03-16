c4bc3d2539b0eebc354a2efb28792314b403a2b3
georgi <gogo249711@gmail.com>
2021-03-16 15:20:32 +0200

    Amend jenkinsfile to run docker

--- pylint.sh
+++ pylint.sh
@@ -6,7 +6,7 @@ out=$(pylint /JenkinsTest/test.py)
 echo $out
 NUMBER=$(echo $out | tr -dc '0-9')
 echo $NUMBER
-extractedNum=$(echo $(NUMBER / 100))
+extractedNum=$(echo $NUMBER / $100)
 echo $extractedNum
 #if[[$out =~ \d+ ]]; then
 #    echo "$out contains a digit"
