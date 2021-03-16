9a651047e1db93aecee3827d7eaac1e466c28098
georgi <gogo249711@gmail.com>
2021-03-16 11:34:15 +0200

    Amend jenkinsfile to run docker

--- pylint.sh
+++ pylint.sh
@@ -4,7 +4,7 @@
 
 out=$(pylint /JenkinsTest/test.py)
 echo $out
-out=$(expr match "$string" '2013:06:23 \([0-9]\+\)')
-echo $out
+outReg=$(expr match "$string" '2013:06:23 \([0-9]\+\)')
+echo $outReg
 #result=$(grub-md5-crypt 2>&1 | grep xy)
 #echo $result > output
