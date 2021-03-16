9251ba90e40cede9c56f4f8c3bf59e72161e0971
georgi <gogo249711@gmail.com>
2021-03-16 16:20:38 +0200

    Amend jenkinsfile to run docker

--- pylint.sh
+++ pylint.sh
@@ -8,6 +8,15 @@ NUMBER=$(echo $out | tr -dc '0-9')
 echo $NUMBER
 extractedNum=$(($NUMBER/100))
 echo $extractedNum
+
+if [extractedNum < 1000]
+then 
+    echo "does not pass"
+    exit 1
+else
+    echo "passes"
+    exit 0
+fi
 #if[[$out =~ \d+ ]]; then
 #    echo "$out contains a digit"
 #else
